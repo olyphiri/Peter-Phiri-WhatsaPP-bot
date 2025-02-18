c5b62205c1a8f088189410338a9286009b825415
PETER PHIRI <swadamonia@gmail.com>
2025-02-18 13:54:25 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Peter Bot",
+  botOwner: 'Olympus Phiri', // Your name
+  ownerNumber: '263784140899', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
